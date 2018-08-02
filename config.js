516c7393f43006f5918d25b9ed9d2b727f64ae91
Banan27240 <42032740+Banan27240@users.noreply.github.com>
2018-08-02 13:02:31 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.discord = {};
+config.discord.email = "banan27240@gmail.com";
+config.discord.password = "04101974";
+
+config.wolfram = {};
+config.wolfram.APP_ID = "2P2VKW-R6KAY2RL3P";
+
+module.exports = config;
